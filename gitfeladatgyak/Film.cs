70a10653fe052f4d2e2219b50e2f3a9b087f33d0
KLaszlo24 <krajczlaci234@gmail.com>
2025-02-07 12:35:38 +0100

    A Film osztály kész. 2 konstruktorral és 3 metódussal

---
+++ gitfeladatgyak/Film.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitfeladatgyak
+{
+	internal class Film
+	{
+		private string cim;
+		private string rendezo;
+		private int hosszPercekben;
+		private string mufaj;
+		private bool megjelent;
+
+		public string Cim { get => cim; set => cim = value; }
+		public string Rendezo { get => rendezo; set => rendezo = value; }
+		public int HosszPercekben { get => hosszPercekben; set => hosszPercekben = value; }
+		public string Mufaj { get => mufaj; set => mufaj = value; }
+		public bool Megjelent { get => megjelent; set => megjelent = value; }
+
+		public Film(string cim, string rendezo, int hosszPercekben, string mufaj, bool megjelent)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			this.hosszPercekben = hosszPercekben;
+			this.mufaj = mufaj;
+			this.megjelent = megjelent;
+		}
+
+		public Film(string cim, string rendezo)
+		{
+			this.cim = cim;
+			this.rendezo = rendezo;
+			hosszPercekben = 120;
+			mufaj = "ismeretlen";
+			megjelent = false;
+
+		}
+
+		public void Jatszas()
+		{
+			if (megjelent == true)
+			{
+                Console.WriteLine("A film elkezdődött.");
+            }
+			else
+			{
+                Console.WriteLine("A film nem kezdődött el");
+            }
+               
+		}
+		public void MufajModositas(string ujMufaj)
+		{
+			mufaj = ujMufaj;
+        }
+
+		public int Hossznoveles(int percek)
+		{
+			return hosszPercekben += percek;
+		}
+
+		public override string? ToString()  // ? - ha null érték lenne akkor is visszadana
+		{
+			return $"{cim} - {rendezo} / {hosszPercekben} / {mufaj} - {megjelent}";
+		}
+
+
+
+	}
+}
--- gitfeladatgyak/Program.cs
+++ gitfeladatgyak/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace gitfeladatgyak
+﻿using System;
+
+namespace gitfeladatgyak
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Könyv konyv1 = new("A kis herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
+
+            Console.WriteLine("Az első feladat-Könyv");
+            Könyv konyv1 = new("A kis herceg", "Antoine de Saint-Exupéry", 1943, 96, 2500);
 			Könyv konyv2 = new("Ez még készül", "Én"); //kijelöl, quick action és new
 
 
@@ -14,7 +18,32 @@
 			konyv1.Arnoveles(1000);
 			
 
-			Console.WriteLine( "\n" + konyv1.ToString() + "\n" + konyv1.HanyoldalMaradt(20)); ;
+			Console.WriteLine( "\n" + konyv1.ToString() + "\n" + konyv1.HanyoldalMaradt(20));
+
+            Console.WriteLine();
+
+            Console.WriteLine("A második feladat-Film");
+
+            Film film1 = new("Egy igazán dühös ember", "Guy Ritchie", 119, "akciófilm", true);
+			Film film2 = new("Ez még készülő film", "Én"); //kijelöl, quick action és new
+
+
+			Console.WriteLine(film1.ToString() + "\n" + film2.ToString());
+            Console.WriteLine();
+            Console.WriteLine(film1.Cim);
+			film1.Jatszas();
+            Console.WriteLine();
+            Console.WriteLine(film2.Cim);
+			film2.Jatszas();
+			film1.MufajModositas("thriller");
+			film2.MufajModositas("vígdráma");
+
+            Console.WriteLine();
+
+
+            Console.WriteLine("Változtatott adatok: ");
+            Console.WriteLine("\n" + film1.ToString() + "\n" + "új hossz:" + film1.Hossznoveles(50));
+			Console.WriteLine("\n" + film2.ToString() + "\n" + "új hossz:" + film2.Hossznoveles(60));
 		}
 	}
 }
